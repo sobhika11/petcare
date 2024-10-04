53f7539fad5269df25e003e57f4dbb007117ffc5
Sobhika <sobhika1105@gmail.com>
2024-10-04 13:16:13 +0530

    Auto commit 16

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite'
 
+
+
 import react from '@vitejs/plugin-react'
+
+
+
+
 
 
 
 export default defineConfig({
 
+
+
   plugins: [react()],
 
+
+
 })
+
+
+
+
 
 
 
