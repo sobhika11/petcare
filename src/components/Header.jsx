d96e3f68164112b341559168b23a89a1b67252fd
Sobhika <sobhika1105@gmail.com>
2024-10-06 14:29:10 +0530

    Auto commit 21

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
+
 import '../styles/navbar.css';
 
+
+
 const Header = () => {
+
   return (
+
     <header>
+
       <h2 style={{ marginRight: '-30px' }}>𝐏𝐞𝐭𝐂𝐚𝐫𝐞.𝐠𝐨 🐾</h2>
 
+
+
       <nav className="navbar">
+
         <ul>
+
           <li><a href="main.html">Home</a></li>
+
           <li><a href="about.html">About Us</a></li>
+
           <li><a href="d.html">Services</a></li>
+
           <li><a href="login.html" className="login-btn">Login / Sign In</a></li>
+
         </ul>
+
       </nav>
+
     </header>
+
   );
+
 };
 
+
+
 export default Header;
+
+
 
