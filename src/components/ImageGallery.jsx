e80191bd27d67f892a2eac0d8c42b85fa284280c
Sobhika <sobhika1105@gmail.com>
2024-10-06 15:32:15 +0530

    Auto commit 23

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,22 +1,44 @@
 // Auto commit 19
+
 import React from 'react';
 
+
+
 const ImageGallery = () => {
+
   return (
+
     <section>
+
       <table cellPadding="10">
+
         <tr>
+
           <td><img src="Images/main3.jpg" width="400px" alt="Pet care" /></td>
+
           <td><img src="Images/main2.avif" width="400px" alt="Pet care" /></td>
+
         </tr>
+
         <tr>
+
           <td><img src="Images/main4.avif" width="400px" alt="Pet care" /></td>
+
           <td><img src="Images/main5.jpg" width="400px" alt="Pet care" /></td>
+
         </tr>
+
       </table>
+
     </section>
+
   );
+
 };
 
+
+
 export default ImageGallery;
+
+
 
