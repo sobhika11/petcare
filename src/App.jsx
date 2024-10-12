c4821c879ec33932ff4c39f3ba0db64a6491f53d
Sobhika <sobhika1105@gmail.com>
2024-10-12 10:23:51 +0530

    Auto commit 45

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,76 @@
 // Auto commit 13
 
+
+
 // Auto commit 14
+
+
 
 import React from 'react';
 
+
+
 import Header from './components/Header';
+
+
 
 import HeroSection from './components/HeroSection';
 
+
+
 import ImageGallery from './components/ImageGallery';
+
+
 
 import '../main.css';
 
 
 
+
+
+
+
 function App() {
+
+
 
   return (
 
+
+
     <div className="App">
+
+
 
       <Header />
 
+
+
       <HeroSection />
+
+
 
       <ImageGallery />
 
+
+
     </div>
 
+
+
   );
+
+
 
 }
 
 
 
+
+
+
+
 export default App;
+
+
 
