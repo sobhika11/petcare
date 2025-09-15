a938b2713735bc4d1ac280c526169e336a0d3869
sobhika11 <sobhika1105@gmail.com>
2025-09-16 00:03:15 +0530

    Update project files

---
+++ Backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+const userSchema=new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        phone_number:{
+            type:Number,
+            required:true
+        },
+        pets_name:{
+            type:String,
+            required:false
+        }
+    });
+module.exports=mongoose.model('user',userSchema);
