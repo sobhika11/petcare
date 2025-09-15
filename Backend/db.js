ec9ac77e241b3a4f81de542238a821e74a9e86cc
sobhika11 <sobhika1105@gmail.com>
2025-09-16 00:14:30 +0530

    Updated Home page

--- Backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("✅ MongoDB connected successfully"))
-.catch((err) => console.error("❌ MongoDB connection failed:", err));
