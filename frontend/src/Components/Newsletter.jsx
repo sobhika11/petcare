f8017944b1612f23728e64fb69c2f908486a0837
Sobhika <sobhika1105@gmail.com>
2026-01-28 10:53:40 +0530

    footer code changed

--- frontend/src/Components/Newsletter.jsx
+++ frontend/src/Components/Newsletter.jsx
@@ -50,7 +50,7 @@ const Newsletter = () => {
   </div>
 
   <div className="footer-bottom">
-    © 2022 petcarego.com All Rights Reserved.
+    © 2022 petcarego.com All Rights Reserved
   </div>
 </footer>
 
