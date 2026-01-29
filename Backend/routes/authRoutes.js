980b4b93142f4f8f1f91b0d0a3f3846210ec0dee
Sobhika <sobhika1105@gmail.com>
2026-01-29 23:35:46 +0530

    login in  logic done

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -35,37 +35,37 @@ router.post('/signup', async (req, res) => {
     res.status(401).json({ message: 'Invalid Google token' })
   }
 })
-// router.post('/login', async (req, res) => {
-//   const { token } = req.body
+router.post('/login', async (req, res) => {
+  const { token } = req.body
 
-//   try {
-//     const ticket = await client.verifyIdToken({
-//       idToken: token,
-//       audience: process.env.CLIENT_ID
-//     })
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.CLIENT_ID
+    })
 
-//     const payload = ticket.getPayload()
-//     const { email } = payload
+    const payload = ticket.getPayload()
+    const { email } = payload
 
-//     const user = await User.findOne({ email })
+    const user = await User.findOne({ email })
 
-//     if (!user) {
-//       return res.status(404).json({ message: 'User not found, please sign up' })
-//     }
+    if (!user) {
+      return res.status(404).json({ message: 'User not found, please sign up' })
+    }
 
-//     const jwtToken = generateToken(user)
+    const jwtToken = generateToken(user)
 
-//     res.status(200).json({
-//       message: 'Login successful',
-//       token: jwtToken,
-//       user: {
-//         id: user._id,
-//         name: user.name,
-//         email: user.email
-//       }
-//     })
-//   } catch (err) {
-//     res.status(401).json({ message: 'Invalid Google token' })
-//   }
-// })
+    res.status(200).json({
+      message: 'Login successful',
+      token: jwtToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    })
+  } catch (err) {
+    res.status(401).json({ message: 'Invalid Google token' })
+  }
+})
 
