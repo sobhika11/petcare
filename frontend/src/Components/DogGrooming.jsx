3226a3a981b5918c22d2b6e6ccee6e975ba73914
Sobhika <sobhika1105@gmail.com>
2026-02-25 12:08:59 +0530

    grooming booking logic

--- frontend/src/Components/DogGrooming.jsx
+++ frontend/src/Components/DogGrooming.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 function Components(props){
+  const [trigger,setTrigger]=useState(false);
+  const Navigate=useNavigate();
   return(
     <>
+
    <div className='card'>
-    <img src={props.img} />
+    <img src={props.img}/>
     <p>{props.text}</p>
-    <button>Book Now</button>
+    <button onClick={()=>{setTrigger(true)
+                          Navigate("/Popup")
+    }
+                        
+    }>Book Now</button>
    </div>
   </>
   )
@@ -14,9 +23,7 @@ function Components(props){
 const DogGrooming = () => {
   return (
     <>
-    <div >
     
-
       <div className='booking'>
       <div className='booking-inner-div'>
       <Components img="../../public/Images/anidcare2.jpg" 
@@ -28,10 +35,20 @@ const DogGrooming = () => {
       <Components img="../../public/Images/anidcare3.avif"
                   text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
           />
+        
+        <br/>
+        <Components img="../../public/Images/anidcare3.avif"
+                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
+          />
+        <Components img="../../public/Images/anidcare3.avif"
+                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
+          />
+          <Components img="../../public/Images/anidcare3.avif"
+                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
+          />
       </div>
       </div>
 
-    </div>
     </>
   )
 }
