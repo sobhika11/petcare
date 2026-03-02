bed60684d1ba3b363375b47894ae20d089def1aa
Sobhika <sobhika1105@gmail.com>
2026-03-02 13:50:38 +0530

    Profile fetching logic

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-
-const Profile = () => {
+import { useNavigate } from "react-router-dom";
+const Profile = ({setLogged}) => {
   const [user,setUser] = useState(null);
-
+  const Navigate=useNavigate();
   useEffect(() => {
     const fetchProfile = async () => {
       const token = localStorage.getItem("token");
-
       const res = await fetch(
         "http://localhost:5000/api/auth/profile",
         {
@@ -15,9 +14,7 @@ const Profile = () => {
           }
         }
       );
-
       const data = await res.json();
-      console.log(token);
       setUser(data);
     };
     console.log(user);
@@ -25,15 +22,35 @@ const Profile = () => {
   }, []);
 
   if(!user) return <h2>Loading...</h2>;
-
+  
   return (
-    <div>
-      <h3>Profile</h3>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
-      <p>Location: {user.location}</p>
+  <div className="profile-container">
+    <div className="userprofile">
+
+      <img
+        src="../public/Images/miss.png"
+        alt="profile"
+        className="profile-avatar"
+      />
+
+      <h3>My Profile</h3>
+
+      <p><strong>Name:</strong> {user.name}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+      <p><strong>Location:</strong> {user.location}</p>
+      <p><strong>Appointment:</strong> {user.appointment || "None"}</p>
+
+      <button onClick={()=>{
+        localStorage.clear();
+        setLogged(false);
+        Navigate("/");
+      }}>
+        Log out
+      </button>
+
     </div>
-  );
+  </div>
+);
 };
 
 export default Profile;
