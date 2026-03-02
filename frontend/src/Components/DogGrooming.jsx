40257bd99b161a5609c74b5f8e69a546d5693c65
Sobhika <sobhika1105@gmail.com>
2026-03-03 00:12:11 +0530

    dog grroming pictures are added

--- frontend/src/Components/DogGrooming.jsx
+++ frontend/src/Components/DogGrooming.jsx
@@ -8,10 +8,11 @@ function Components(props){
   return(
     <>
    <div className='card'>
+    <p className='title'>{props.title}</p>
     <img src={props.img}/>
     <p>{props.text}</p>
     <button onClick={()=>{setTrigger(true)
-                          Navigate("/Popup")
+                          Navigate("/Popup",{state:{servicename:props.title}})
     }}>Book Now</button>
    </div>
   </>
@@ -25,25 +26,37 @@ const DogGrooming = () => {
     
       <div className='booking'>
       <div className='booking-inner-div'>
-      <Components img="../../public/Images/anidcare2.jpg" 
-                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation "
+      <Components title="Paw trim"
+                  img="../../public/Images/dog hair cut_groom.webp" 
+                  text="Expert trimming for a clean and stylish coat.
+Keeps your dog cool, neat, and comfortable.🐶"
           />
       <Components img="../../public/Images/anidcare3.avif"
-                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
+                  text="Gentle grooming tailored for your feline friend.
+Soft, smooth, and perfectly shaped fur.🐱"
+                  title="kitty cut"
           />
-      <Components img="../../public/Images/anidcare3.avif"
-                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
+      <Components img="../../public/Images/vet3.jpeg"
+                  text="Safe, pet-friendly colors for a bold new look.
+Adds shine and fun to your dog’s coat.🐾"
+                  title="Paw tint"
           />
         
         <br/>
         <Components img="../../public/Images/anidcare3.avif"
-                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
+                  text="Carefully applied color for a graceful finish.
+Enhances your cat’s natural elegance.🍃"
+                  title="kitty tint"
           />
-        <Components img="../../public/Images/anidcare3.avif"
-                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
+        <Components img="../../public/Images/vet2.jpeg"
+                  text="Includes a refreshing bath and gentle nail trimming.
+A complete clean-up for a polished, adorable look.✨."
+                  title="Pet Glow-Up"
           />
           <Components img="../../public/Images/anidcare3.avif"
-                  text="In Java, the long data type is a 64-bit signed integer that uses two’s complement representation."
+                  text="Loving supervision and attention throughout the day.
+Playtime, comfort, and proper care while you're away.💕"
+                  title="Pet Caretaker"
           />
       </div>
       </div>
