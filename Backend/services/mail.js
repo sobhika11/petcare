7f40517da65e3986284237876e7eed4f56d8445f
Sobhika <sobhika1105@gmail.com>
2026-03-06 00:54:28 +0530

    Accessed gmail and password  with env file

--- Backend/services/mail.js
+++ Backend/services/mail.js
@@ -2,25 +2,26 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  auth: {
-    user: "sobhika1105@gmail.com",     
-    pass: "gqijogfuwoilxhzl"        
-  }
+ auth: {
+  user: process.env.EMAIL_USER,
+  pass: process.env.EMAIL_PASS
+}
 });
 const sendEmail = async (to, subject, text) => {
   try {
-
     const mailOptions = {
       from: "sobhika1105@gmail.com",
       to: to,
       subject: subject,
-      text: text
+      text: text,
+      messageId: `<${Date.now()}@yourdomain.com>`, 
+      headers: {
+        'X-Entity-Ref-ID': Date.now().toString() // Specific header to break threads
+      }
     };
 
     const info = await transporter.sendMail(mailOptions);
-
     console.log("Email sent: " + info.response);
-
   } catch (error) {
     console.log("Email error:", error);
   }
